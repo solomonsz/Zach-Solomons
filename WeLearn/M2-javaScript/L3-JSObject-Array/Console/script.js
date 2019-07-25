4cd5e7fe940dd7da541f04eb0d674f8d9f4cb58e
Zachary Solomons <solom020@csusm.edu>
2019-07-25 10:15:02 -0700

    comit

---
+++ WeLearn/M2-javaScript/L3-JSObject-Array/Console/script.js
@@ -0,0 +1,25 @@
+const article = {
+  name: 'Dog family gives birth to 10 puppies',
+  views: 1035,
+  datePublished: '03/25/2018',
+  author: {
+    name: 'Joe Corgi',
+    title: 'Senior Canine Editor',
+  }
+};
+const computer = {
+  name: 'Dell Computer',
+  storage: '8',
+};
+const strains = {
+  strain1: 'Pineapple Express',
+  strain2: 'Grandady Purp',
+  strain3: 'OG Kush',
+  strain4: 'Daryl Strawberry',
+  strain5: 'Sour Diesel',
+};
+console.log(strains.strain1);
+console.log(strains.strain2);
+console.log(strains.strain3);
+console.log(strains.strain4);
+console.log(strains.strain5);
